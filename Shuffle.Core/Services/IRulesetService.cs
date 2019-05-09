1c97c73e77b983870230d8ae9cdf28b4ff3f01f0
Tim McGee <tmcgee@reliaslearning.com>
2019-05-09 14:25:50 -0400

    Adding ruleset stuff

---
+++ Shuffle.Core/Services/IRulesetService.cs
@@ -0,0 +1,14 @@
+﻿using Shuffle.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shuffle.Core.Services
+{
+    public interface IRulesetService
+    {
+        List<Ruleset> GetRulesets();
+        Ruleset GetRuleset(int id);
+    }
+}
---
+++ Shuffle.Core/Services/RulesetService.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using AutoMapper.QueryableExtensions;
+using Shuffle.Data;
+using Shuffle.Data.Entities;
+using Shuffle.Core.Models;
+
+namespace Shuffle.Core.Services
+{
+    public class RulesetService : IRulesetService
+    {
+        private readonly ShuffleDbContext _db;
+
+        public RulesetService(ShuffleDbContext context)
+        {
+            _db = context;
+        }
+
+        public Ruleset GetRuleset(int id)
+        {
+            var ruleset = _db.Rulesets.Where(x => x.Id == id).ProjectTo<Ruleset>().FirstOrDefault();
+
+            return ruleset ;
+        }
+
+        public List<Ruleset> GetRulesets()
+        {
+            var ruleset = _db.Rulesets.ProjectTo<Ruleset>().ToList();
+
+            return ruleset;
+        }
+
+    }
+}
