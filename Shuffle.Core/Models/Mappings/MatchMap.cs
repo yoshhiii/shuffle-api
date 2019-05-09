7a05bbaa1ecc044a7389af77b4c9a9e549ba1380
Chris Walsh <cwalsh@reliaslearning.com>
2019-05-09 15:08:07 -0400

    getting scores from service

---
+++ Shuffle.Core/Models/Mappings/MatchMap.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using AutoMapper;
+using Shuffle.Data.Entities;
+using Shuffle.Core.Models;
+
+namespace shuffleboard.core.Models.Mappings
+{
+    public class MatchMap : Profile
+    {
+        public MatchMap()
+        {
+            CreateMap<MatchEntity, Match>()
+                .ForMember(x => x.Id, opt => opt.MapFrom(x => x.Id))
+                .ForMember(x => x.ChallengerId, opt => opt.MapFrom(x => x.ChallengerId))
+                .ForMember(x => x.ChallengerName, opt => opt.MapFrom(x => x.Challenger.Name))
+                .ForMember(x => x.ChallengerScore, opt => opt.MapFrom(x => x.ChallengerScore))
+                .ForMember(x => x.OppositionId, opt => opt.MapFrom(x => x.OppositionId))
+                .ForMember(x => x.OppositionName, opt => opt.MapFrom(x => x.Opposition.Name))
+                .ForMember(x => x.OppositionScore, opt => opt.MapFrom(x => x.OppositionScore))
+                .ForMember(x => x.MatchDate, opt => opt.MapFrom(x => x.MatchDate))
+                .ForMember(x => x.RulesetId, opt => opt.MapFrom(x => x.RulesetId));
+        }
+    }
+}
--- Shuffle.Core/Models/Match.cs
+++ Shuffle.Core/Models/Match.cs
@@ -8,8 +8,10 @@ namespace Shuffle.Core.Models
         public int Id { get; set; }
         public int ChallengerId { get; set; }
         public int OppositionId { get; set; }
-        public int TeamScore1 { get; set; }
-        public int TeamScore2 { get; set; }
+        public int ChallengerScore { get; set; }
+        public int OppositionScore { get; set; }
+        public string ChallengerName { get; set; }
+        public string OppositionName { get; set; }
         public DateTime MatchDate { get; set; }
         public int RulesetId { get; set; }
     }
