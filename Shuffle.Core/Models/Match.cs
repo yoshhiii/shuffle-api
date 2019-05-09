6c1e23d41a99365309d70b2e73a0242bd38e5f8b
Tim McGee <tmcgee@reliaslearning.com>
2019-05-09 16:33:54 -0400

    asdf

--- Shuffle.Core/Models/Match.cs
+++ Shuffle.Core/Models/Match.cs
@@ -5,11 +5,11 @@ namespace Shuffle.Core.Models
 {
     public class Match
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public int ChallengerId { get; set; }
         public int OppositionId { get; set; }
-        public int ChallengerScore { get; set; }
-        public int OppositionScore { get; set; }
+        public int? ChallengerScore { get; set; }
+        public int? OppositionScore { get; set; }
         public string ChallengerName { get; set; }
         public string OppositionName { get; set; }
         public DateTime MatchDate { get; set; }
