a28ac50c0002c89f0a824b27cc902e7ac31d9d55
Chris Sullivan <csullivan@reliaslearning.com>
2019-05-09 23:06:02 -0400

    fixed

--- Shuffle.Core/Services/ITeamRecordService.cs
+++ Shuffle.Core/Services/ITeamRecordService.cs
@@ -8,11 +8,7 @@ namespace Shuffle.Core.Services
 {
     public interface ITeamRecordService
     {
-<<<<<<< HEAD
         List<TeamRecord> GetTeamRecords(int? rulesetId);
-=======
-        List<TeamRecord> GetTeamRecords(int? Id);
->>>>>>> 9b209244f3eb151307a73a1bdef72e2ef39ed9e5
         TeamRecord GetTeamRecord(int Id);
         TeamRecord CreateTeamRecord(TeamRecord recordToCreate);
     }
